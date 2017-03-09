0fd051c9e89e553dc1a56900284dd141dfedfd31
yellow9697 <warham1@naver.com>
2017-03-09 15:24:20 +0900

    ver2

--- source.c
+++ source.c
@@ -3,6 +3,7 @@
 int main()
 {
 	char* str = "이태식";
+	char* str2 = "학번은 2011037257 입니다.";
 
 	int input = 0;
 
@@ -11,6 +12,10 @@ int main()
 	if(input == 1)
 	{
 		printf("%s", str);
+	}
+	else if(input == 2)
+	{
+		printf("%s", str2);
 	}
 
 	return 0;
